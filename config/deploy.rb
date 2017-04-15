3dcc6a96ac3bcddd9638ca5438a701945d13a9b2
Vyacheslav Kuzharov <vyacheslav.kuzharov@umbrella-web.com>
2017-04-15 22:07:48 +0300

    Prepare Capistrano configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,8 +2,8 @@
 lock '3.8.0'
 
 
-set :repo_url,        'git@example.com:username/appname.git'
-set :application,     'appname'
+set :repo_url,        'git@github.com:VyacheslavKuzharov/tagnotes.git'
+set :application,     'tagnotes'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
@@ -31,7 +31,7 @@ set :log_level,     :debug
 set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml config/application.yml}
 set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
@@ -47,16 +47,6 @@ namespace :puma do
 end
 
 namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
-  end
 
   desc 'Initial Deploy'
   task :initial do
@@ -68,12 +58,11 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
+    on roles(:app), in: :sequence, wait: 3 do
       invoke 'puma:restart'
     end
   end
 
-  before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
